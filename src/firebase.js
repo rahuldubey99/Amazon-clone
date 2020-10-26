067497d3c18dae7d01d02888653f0b8bde7c7fce
ahuldubeycse <rahulsharma9451660093@gmail.com>
2020-10-26 20:50:32 +0530

    navbar

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCTjpmN7LPXob43SX1JMZyVhn4TtA7X2Tc",
+    authDomain: "e-challenge-2dbd2.firebaseapp.com",
+    databaseURL: "https://e-challenge-2dbd2.firebaseio.com",
+    projectId: "e-challenge-2dbd2",
+    storageBucket: "e-challenge-2dbd2.appspot.com",
+    messagingSenderId: "831064845828",
+    appId: "1:831064845828:web:8c3e104c60b94866ddcf51",
+    measurementId: "G-VPM317WHEZ"
+  };

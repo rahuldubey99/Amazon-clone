7a8e8d33be15e58dab2fc4673e969385cd0dd50d
root <rahulsharm9451660093@gmail.com>
2020-10-27 17:58:52 +0530

    product page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import Header from './Header.js';
 import  './Header.css';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-       {/* Header */}
-       <Header/>
-       {/* Home */}
-        <Home/>       
-    </div>
+    <Router>
+      <div className="App">
+          <Switch> 
+          <Route path='/checkout'>
+              <Header/>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+           </Switch>
+      </div>   
+    </Router> 
   );
 }
 

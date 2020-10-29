46aed9d465858b0bcf717c132a33ea2de4201039
root <rahulsharm9451660093@gmail.com>
2020-10-29 16:26:49 +0530

    Checkout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 import Header from './Header.js';
-import  './Header.css';
+import './Header.css';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout';
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-          <Switch> 
+        <Switch>
           <Route path='/checkout'>
-              <Header/>
+            <Header />
+            <Checkout />
           </Route>
           <Route path="/">
             <Header />
             <Home />
           </Route>
-           </Switch>
-      </div>   
-    </Router> 
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,39 +1,45 @@
 import React from 'react'
 import SearchIcon from '@material-ui/icons/Search';
 import { ShoppingBasket } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import { useStateValue } from "./StateProvider";
 function Header() {
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
         <div className="header">
-            
-            <img alt="amazonlogo" 
-             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            className='header__logo'/>
+            <Link to="/">
+                <img alt="amazonlogo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    className='header__logo' />
+            </Link>
             <div className="header__search">
-            
-                <input type="text" className="header__searchInput"/>
-                <SearchIcon className="header__searchIcon "/>
-            </div>  
+
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon " />
+            </div>
             <div className="header__nav">
                 <div className="header__option">
-                   <span className="header__optionLineOne">Hello Guest</span>
-                    
-                   <span className="header__optionLineTwo">Sign In</span>
-                    
+                    <span className="header__optionLineOne">Hello Guest</span>
+
+                    <span className="header__optionLineTwo">Sign In</span>
+
                 </div>
                 <div className="header__option">
-                <span className="header__optionLineOne"> Returns</span>
-                    
+                    <span className="header__optionLineOne"> Returns</span>
+
                     <span className="header__optionLineTwo">& Orders</span>
                 </div>
                 <div className="header__option">
-                <span className="header__optionLineOne">Your</span>
-                    
+                    <span className="header__optionLineOne">Your</span>
+
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
-                <div className="header__optionBasket">
-                    <ShoppingBasket/>
-                    <span className="header__optionLineTwo header__basketCount">0</span>
-                </div>
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasket />
+                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
